c875312a161c5803478b6e0d52513fbcc57947b3
tuannt <>
2017-11-01 17:20:39 +0700

    update ccode

--- flashLight/flashLight/ViewController.swift
+++ flashLight/flashLight/ViewController.swift
@@ -9,16 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  //MARK: UI elements
+  @IBOutlet weak var color_preview: UIView!
+  @IBOutlet weak var textField_red: UITextField!
+  @IBOutlet weak var slider_red: UISlider!
+  @IBOutlet weak var textField_green: UITextField!
+  @IBOutlet weak var slider_green: UISlider!
+  @IBOutlet weak var textField_blue: UITextField!
+  @IBOutlet weak var slider_blue: UISlider!
+  @IBOutlet weak var textField_alpha: UITextField!
+  @IBOutlet weak var slider_alpha: UISlider!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
