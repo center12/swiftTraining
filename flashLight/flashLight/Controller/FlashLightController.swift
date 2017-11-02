dae26391b33da9f8946065b7c9db658217fb41d2
tuannt <>
2017-11-02 17:20:37 +0700

    update code

--- flashLight/flashLight/Controller/FlashLightController.swift
+++ flashLight/flashLight/Controller/FlashLightController.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+import UIKit
+
+class FlashLightController: UIViewController {
+  //MARK: UI elements
+  @IBOutlet weak var colorView: UIView!
+  @IBOutlet weak var txtRed: UITextField!
+  @IBOutlet weak var sliderRed: UISlider!
+  @IBOutlet weak var txtGreen: UITextField!
+  @IBOutlet weak var sliderGreen: UISlider!
+  @IBOutlet weak var txtBlue: UITextField!
+  @IBOutlet weak var sliderBlue: UISlider!
+  @IBOutlet weak var txtAlpha: UITextField!
+  @IBOutlet weak var sliderAlpha: UISlider!
+  
+  //MARK: action
+  @IBAction func sliderRed_ValueChange(_ sender: UISlider) {
+    
+  }
+  @IBAction func sliderGreen_ValueChange(_ sender: UISlider) {
+    
+  }
+  @IBAction func sliderBlue_ValueChange(_ sender: UISlider) {
+  
+  }
+  @IBAction func sliderAlpha_ValueChange(_ sender: UISlider) {
+    
+  }
+  
+  //MARK: ViewController
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
